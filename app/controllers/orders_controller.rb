fa5fa949ae93cef73ada7d387d934fc8761afeba
putu chandra <chandevcode@gmail.com>
2025-11-29 15:24:27 +0800

    feat: checkout

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,26 @@
+class OrdersController < ApplicationController
+  def create
+    cart = session[:cart] || {}
+
+    if cart.empty?
+      redirect_to root_path, alert: "Your cart is empty" and return
+    end
+    total_price = cart.sum { |_, item| item["qty"] * item["price"]}
+
+    order = Order.create!(total_price: total_price, status: "success")
+
+    #create each order item
+    cart.each do |menu_item_id, item |
+      OrderItem.create!(
+        order: order,
+        menu_item_id: menu_item_id,
+        qty: item["qty"],
+        price: item["qty"] * item["price"],
+      )
+    end
+
+    # clear cart
+    session[:cart] = {}
+    redirect_to root_path, notice: "Order Created"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   resources :menu_items
   get "home/index"
   resources :categories
+  #resources :orders, only: [:show]
 
 
   post "carts/add/:id", to: "carts#add", as: "add_to_cart"
+  post "/checkout", to: "orders#create", as: "checkout"
 
   delete "carts/remove/:id", to: "carts#remove", as: "remove_cart"
   root "home#index"
